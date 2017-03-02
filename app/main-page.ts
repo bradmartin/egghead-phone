4b0fcb8ddd5af3f62c519f3510f609485ddb6447
bradmartin <bradwaynemartin@gmail.com>
2017-03-01 22:54:32 -0600

    go

--- app/main-page.ts
+++ app/main-page.ts
@@ -1,7 +1,7 @@
 import * as phone from 'nativescript-phone';
 
 export function callHome() {
-    phone.sms(['615-123-4567', '615-474-2929'], 'Hello egghead')
+    phone.sms(['615-123-1234', '615-123-4858'], 'Hello egg head')
         .then((result) => {
             console.log(result);
         })
